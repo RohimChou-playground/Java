16ffc352171862eddf5bacd6686f4a60ddbde7cf
RohimChou <rohim.chou@gmail.com>
2022-09-05 00:45:50 +0800

    005/02/Logback HelloLogback

--- 005-SpringBoot1/src/main/java/com/rohim/springboot1/controllers/TestController.java
+++ 005-SpringBoot1/src/main/java/com/rohim/springboot1/controllers/TestController.java
@@ -1,5 +1,7 @@
 package com.rohim.springboot1.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,9 +11,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("test")
 public class TestController {
 	
+	private Logger logger1 = LoggerFactory.getLogger(TestController.class);
+	
 	@RequestMapping(path = "t1", method = RequestMethod.GET)
 	@ResponseBody
 	public Object t1() {
+		this.logger1.error("Hello World From logger1");
 		return "Hello World";
 	}
 }
