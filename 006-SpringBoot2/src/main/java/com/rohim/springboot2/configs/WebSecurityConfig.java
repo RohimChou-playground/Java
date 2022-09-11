e3a5c0896e3f3515fab696615c7cd4174b61baba
RohimChou <rohim.chou@gmail.com>
2022-09-12 01:26:22 +0800

    006/01/Security custom UserDetailsService

--- 006-SpringBoot2/src/main/java/com/rohim/springboot2/configs/WebSecurityConfig.java
+++ 006-SpringBoot2/src/main/java/com/rohim/springboot2/configs/WebSecurityConfig.java
@@ -2,13 +2,15 @@ package com.rohim.springboot2.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.rohim.springboot2.services.UserDetailsServiceImp;
 
 @Configuration
 @EnableWebSecurity
@@ -32,15 +34,18 @@ public class WebSecurityConfig {
 		return http.build();
     }
     
-	@Bean
-	public UserDetailsService userDetailsService() {
-		UserDetails user =
-			 User.withDefaultPasswordEncoder()
-				.username("root")
-				.password("1234")
-				.roles("USER")
-				.build();
-
-		return new InMemoryUserDetailsManager(user);
-	}
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder, UserDetailsServiceImp userDetailsService) 
+      throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+          .userDetailsService(userDetailsService)
+          .passwordEncoder(bCryptPasswordEncoder)
+          .and()
+          .build();
+    }
 }
---
+++ 006-SpringBoot2/src/main/java/com/rohim/springboot2/services/UserDetailsServiceImp.java
@@ -0,0 +1,41 @@
+package com.rohim.springboot2.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.rohim.springboot2.models.User;
+import com.rohim.springboot2.models.UserPrincipal;
+
+@Service
+public class UserDetailsServiceImp implements UserDetailsService {
+
+	@Autowired
+	public PasswordEncoder pwdEncoder;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = this.findUserbyUernameFromDb(username);
+		if (user == null) {
+			throw new UsernameNotFoundException(username);
+		}
+		
+		return new UserPrincipal(user);
+	}
+
+	// pretending getting user from db, though not
+	private User findUserbyUernameFromDb(String username) {
+		if (username.equalsIgnoreCase("root")) {
+			User u = new User();
+			u.setId(1);
+			u.setName("root");
+			u.setPassword(this.pwdEncoder.encode("1234"));
+			
+			return u;
+		}
+		return null;
+	}
+}
