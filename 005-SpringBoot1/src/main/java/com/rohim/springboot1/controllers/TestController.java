362fb646e61560ba1062c12bf80c2e86aeb4f715
RohimChou <rohim.chou@gmail.com>
2022-09-12 13:41:50 +0800

    005/02/Logback MDC

--- 005-SpringBoot1/src/main/java/com/rohim/springboot1/controllers/TestController.java
+++ 005-SpringBoot1/src/main/java/com/rohim/springboot1/controllers/TestController.java
@@ -2,6 +2,7 @@ package com.rohim.springboot1.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,6 +12,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("test")
 public class TestController {
 	
+	public TestController() {
+		System.out.println("TestController created");
+	}
+
 	private Logger logger1 = LoggerFactory.getLogger(TestController.class);
 	
 	@RequestMapping(path = "t1", method = RequestMethod.GET)
@@ -19,5 +24,19 @@ public class TestController {
 		this.logger1.error("Hello World From logger1");
 		this.logger1.debug("Some Debug Info");
 		return "Hello World";
+	}
+	
+	Integer counter = 0;
+	@RequestMapping(path = "t2", method = RequestMethod.GET)
+	@ResponseBody
+	public Object t2() {
+		this.logger1.debug("1 Hello World 2 From logger1");
+		
+		// key is case sensitive
+		// everytime controller is called will be reset
+		MDC.put("firstName", "John" + ++counter);
+		
+		this.logger1.debug("2 Hello World 2 From logger1");
+		return "Hello World 2";
 	}
 }
