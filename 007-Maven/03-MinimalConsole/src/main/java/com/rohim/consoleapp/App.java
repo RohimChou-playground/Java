3503946b060b6aabb00124298f1c32b75518293d
RohimChou <rohim.chou@gmail.com>
2022-11-16 17:38:29 +0800

    json

--- 007-Maven/03-MinimalConsole/src/main/java/com/rohim/consoleapp/App.java
+++ 007-Maven/03-MinimalConsole/src/main/java/com/rohim/consoleapp/App.java
@@ -1,6 +1,13 @@
 package com.rohim.consoleapp;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JsonProcessingException {
+        Employee john = new Employee(1, "John", 29);
+        String json = new ObjectMapper().writeValueAsString(john);
+
+        System.out.println(json);
     }
 }
