bf31c7a7e6b70ef147616fb79b7f70172d151bf2
RohimChou <rohim.chou@gmail.com>
2022-11-16 23:47:05 +0800

    WriteTextFile

--- 007-Maven/03-MinimalConsole/src/main/java/com/rohim/consoleapp/App.java
+++ 007-Maven/03-MinimalConsole/src/main/java/com/rohim/consoleapp/App.java
@@ -1,6 +1,62 @@
 package com.rohim.consoleapp;
 
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 public class App {
     public static void main(String[] args) {
+        String msg = "Text to save to file 22222" + System.lineSeparator();
+
+        // ================================================
+        // ============ First way for doing it ============
+        // ================================================
+        try {
+            // will create a file, if not exist
+            Files.write(
+                    Paths.get("./fileName.txt"),
+                    msg.getBytes(StandardCharsets.UTF_8),
+                    StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+
+            Files.write(
+                    Paths.get("./fileName.txt"),
+                    msg.getBytes(StandardCharsets.UTF_8),
+                    StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+        // ===============================================
+        // =========== Second way for doing it ===========
+        // ===============================================
+        Writer writer = null;
+        try {
+            writer = new BufferedWriter(
+                    new OutputStreamWriter(
+                            new FileOutputStream("filename2.txt"),
+                            StandardCharsets.UTF_8));
+
+            writer.write(msg);
+            writer.write(msg);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                writer.close();
+            } catch (Exception ex) {
+                /* ignore */
+            }
+        }
     }
 }
