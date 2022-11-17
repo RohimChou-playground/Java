8afb14a294f56dbf2aa89d30730614249d78564c
RohimChou <rohim.chou@gmail.com>
2022-11-17 16:29:28 +0800

    ThreadPoolExecutor

--- 007-Maven/03-MinimalConsole/src/main/java/com/rohim/consoleapp/App.java
+++ 007-Maven/03-MinimalConsole/src/main/java/com/rohim/consoleapp/App.java
@@ -1,15 +1,37 @@
 package com.rohim.consoleapp;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Future;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.ThreadPoolExecutor;
+
 public class App {
     public static void main(String[] args) throws InterruptedException {
 
-        Thread thread1 = new Thread(new Task1(), "thread1");
-        Thread thread2 = new Thread(new Task1(), "thread2");
+        // minimum threads 0
+        // maximum threads 5, (the last 3 threads would only be used, if all 6 waiting is full)
+        // maximum tasks in executorService 11=(5 running + 6 waiting)\
+        // threads would be closed when idle more then 3 secs 
+        ArrayBlockingQueue<Runnable> abQueue = new ArrayBlockingQueue<Runnable>(6, true);
+        ThreadPoolExecutor executorService = new ThreadPoolExecutor(
+            2, 5, 3L, TimeUnit.SECONDS, abQueue, new ThreadPoolExecutor.AbortPolicy());
 
-        thread1.start();
-        thread2.start();
+        for (int i = 0; i < 5; i++) {
+            System.out.println("==================" + i + "===================");
 
-        Thread.sleep(3300);
-        thread2.interrupt();
+            try {
+                Future<String> future = executorService.submit(new Task1(i));
+                System.out.println(executorService);
+            } catch (RejectedExecutionException e) {
+                --i;
+                System.out.println(e.getMessage());
+                System.out.println(executorService);
+                Thread.sleep(5000);
+            }
+        }
+
+        Thread.sleep(15000);
+        System.out.println(executorService);
     }
 }
--- 007-Maven/03-MinimalConsole/src/main/java/com/rohim/consoleapp/Task1.java
+++ 007-Maven/03-MinimalConsole/src/main/java/com/rohim/consoleapp/Task1.java
@@ -1,15 +1,26 @@
 package com.rohim.consoleapp;
 
-public class Task1 implements Runnable {
-    public void run() {
+import java.util.concurrent.Callable;
+
+public class Task1 implements Callable<String> {
+
+    private int taskNum;
+
+    public Task1(int taskNum) {
+        this.taskNum = taskNum;
+    }
+
+    public String call() {
         for (int i = 0; i < 5; i++) {
-            System.out.println(Thread.currentThread().getName() + " says: Task1 triggered (" + (i + 1) + ")");
+            System.out.println(Thread.currentThread().getName() + " says: Task" + taskNum + " triggered (" + (i + 1) + ")");
 
             try {
-                Thread.sleep(1500);
+                Thread.sleep(800);
             } catch (InterruptedException e) {
                 System.out.println(Thread.currentThread().getName() + " says: " + e.getMessage());
             }
         }
+
+        return Thread.currentThread().getName() + " says: Task" + taskNum + " succeed";
     }
 }
