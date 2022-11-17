9bd95be8f00d05672cc6c8a42100beca4d51b528
RohimChou <rohim.chou@gmail.com>
2022-11-17 10:55:03 +0800

    Thread Deadlock

---
+++ 007-Maven/03-MinimalConsole/src/main/java/com/rohim/consoleapp/Task1.java
@@ -0,0 +1,19 @@
+package com.rohim.consoleapp;
+
+public class Task1 implements Runnable {
+    public void run() {
+        try {
+            String threadName = Thread.currentThread().getName();
+            synchronized (SharedResource.obj1) {
+                System.out.println(threadName + ": 取得 obj1 的鎖");
+                Thread.sleep(1000);
+                System.out.println(threadName + ": 等待 obj2 的鎖");
+                synchronized (SharedResource.obj2) {
+                    System.out.println(threadName + ": 取得 obj2 的鎖");
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
