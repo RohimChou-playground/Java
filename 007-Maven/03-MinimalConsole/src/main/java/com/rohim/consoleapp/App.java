34e4281a8a014112d7adb04eb1b50c3bba9467bf
RohimChou <rohim.chou@gmail.com>
2022-11-18 14:05:31 +0800

    Spring by XML

--- 007-Maven/03-MinimalConsole/src/main/java/com/rohim/consoleapp/App.java
+++ 007-Maven/03-MinimalConsole/src/main/java/com/rohim/consoleapp/App.java
@@ -1,6 +1,13 @@
 package com.rohim.consoleapp;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
     public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("aaa.xml");
+        UserModel user = (UserModel) context.getBean("user1");
+        System.out.println(user.getName());
+        System.out.println(user.getRole().getName());
     }
 }
---
+++ 007-Maven/03-MinimalConsole/src/main/java/com/rohim/consoleapp/UserModel.java
@@ -0,0 +1,33 @@
+package com.rohim.consoleapp;
+
+public class UserModel {
+    private int id;
+    private String name;
+    private int age;
+    private RoleModel role;
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public RoleModel getRole() {
+        return role;
+    }
+    public void setRole(RoleModel role) {
+        this.role = role;
+    }
+}
