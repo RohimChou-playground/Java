17bc441095a63f5765ec1f658b2f91c0f5b8195d
RohimChou <rohim.chou@gmail.com>
2022-11-18 10:51:31 +0800

    Read Properties

--- 007-Maven/03-MinimalConsole/src/main/java/com/rohim/consoleapp/App.java
+++ 007-Maven/03-MinimalConsole/src/main/java/com/rohim/consoleapp/App.java
@@ -1,6 +1,25 @@
 package com.rohim.consoleapp;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        InputStream inputStream = App.class.getClassLoader().getResourceAsStream("aaa.txt");
+        
+        Properties properties = new Properties();
+        properties.load(inputStream);
+
+        // {abcd=AABBCCDD, 1234=12345678}
+        System.out.println(properties);
+
+        // AABBCCDD
+        System.out.println(properties.getProperty("abcd"));
+
+
+        // default value set by program
+        System.out.println(properties.getProperty("zzzzzzzzzzzz", "default value set by program"));
     }
 }
