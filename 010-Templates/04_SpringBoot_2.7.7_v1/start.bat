aa9244473d7d1f9ff298954642d2dba7374dd1b5
RohimChou <rohim.chou@gmail.com>
2023-03-09 09:50:44 +0800

    Templates: update springboot's .bat

--- 010-Templates/04_SpringBoot_2.7.7_v1/start.bat
+++ 010-Templates/04_SpringBoot_2.7.7_v1/start.bat
@@ -1,7 +1,7 @@
-@開發hotreload用
+@REM for hot reload
 @REM mvn spring-boot:run -Dspring-boot.run.arguments="--spring.config.location='classpath:application-dev.properties'"
 
-@準備部屬用
+@REM for deployment
 call mvn clean package -DskipTests
 rmdir target\antrun /s /q
 rmdir target\classes /s /q
@@ -11,3 +11,6 @@ rmdir target\maven-status /s /q
 del target\web.jar.original /q
 
 java -Dspring.config.location=target\application-dev.properties -jar target\web.jar
+
+@REM for specify custom main class
+@REM java -Dspring.config.location=target\application-dev.properties -Dloader.main=com.rohim.web.ApplicationStart org.springframework.boot.loader.PropertiesLauncher
--- 010-Templates/05_SpringBoot_2.7.7_thymeleaf_v1/start.bat
+++ 010-Templates/05_SpringBoot_2.7.7_thymeleaf_v1/start.bat
@@ -1,3 +1,7 @@
+@REM for hot reload
+@REM mvn spring-boot:run -Dspring-boot.run.arguments="--spring.config.location='classpath:application-dev.properties'"
+
+@REM for deployment
 call mvn clean package -DskipTests
 rmdir target\antrun /s /q
 rmdir target\classes /s /q
@@ -5,4 +9,8 @@ rmdir target\generated-sources /s /q
 rmdir target\maven-archiver /s /q
 rmdir target\maven-status /s /q
 del target\web.jar.original /q
-call mvn spring-boot:run -Dspring-boot.run.arguments="--spring.config.location='classpath:application-dev.properties'"
+
+java -Dspring.config.location=target\application-dev.properties -jar target\web.jar
+
+@REM for specify custom main class
+@REM java -Dspring.config.location=target\application-dev.properties -Dloader.main=com.rohim.web.ApplicationStart org.springframework.boot.loader.PropertiesLauncher
